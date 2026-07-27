8111cda437c947aeea3737244989095a930724b8
Renato Souza <renato.neto@argointeligencia.com.br>
2026-07-27 10:50:01 -0300

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pydantic.main import BaseModel
 
 app = FastAPI()
 
-task1 = {"id": 1, "title": "Task 1", "done": True}
-task2 = {"id": 2, "title": "Task 2", "done": False}
-task3 = {"id": 3, "title": "Task 3", "done": True}
+class Task(BaseModel):
+    id: int
+    title: str
+    done: bool
+
+class TaskCreate(BaseModel):
+    title: str | None = None
+
+tasks = [
+    {"id": 1, "title": "Task 1", "done": True},
+    {"id": 2, "title": "Task 2", "done": False},
+    {"id": 3, "title": "Task 3", "done": True},
+]
 
 @app.get("/")
 async def root():
@@ -17,12 +28,21 @@ async def health():
 
 @app.get("/tasks")
 async def get_tasks():
-    return [task1, task2, task3]
+    return tasks
 
 @app.get("/tasks/{id}")
 async def get_tasks_by_id(id: int):
-    for task in [task1, task2, task3]:
+    for task in tasks:
         if task["id"] == id:
             return task
     
     return JSONResponse(status_code=404,content={"error": f"Task {id} not found"})
+
+@app.post("/tasks", status_code=201)
+async def create_task(task: TaskCreate):
+    if not task.title:
+        return JSONResponse(status_code=400, content={"error": "Title is required."})
+    next_id = max(task["id"] for task in tasks) + 1
+    new_task = {"id": next_id, "title": task.title, "done": False}
+    tasks.append(new_task)
+    return new_task
