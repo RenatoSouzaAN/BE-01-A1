f20e1a9d3b5960dce08336cd899d3e7e4d060f60
Renato Souza <renato.neto@argointeligencia.com.br>
2026-07-27 11:17:04 -0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ class Task(BaseModel):
 class TaskCreate(BaseModel):
     title: str | None = None
 
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    done: bool | None = None
+
 tasks = [
     {"id": 1, "title": "Task 1", "done": True},
     {"id": 2, "title": "Task 2", "done": False},
@@ -46,3 +50,27 @@ async def create_task(task: TaskCreate):
     new_task = {"id": next_id, "title": task.title, "done": False}
     tasks.append(new_task)
     return new_task
+
+@app.put("/tasks/{id}")
+async def update_task(id: int, task: TaskUpdate):
+    if not task.title and not task.done is not None:
+        return JSONResponse(status_code=400, content={"error": "At least one field to update is required."})
+    for t in tasks:
+        if t["id"] == id:
+            if task.title:
+                t["title"] = task.title
+
+            if task.done is not None:
+                t["done"] = task.done
+            return t
+    
+    return JSONResponse(status_code=404, content={"error": f"Task {id} not found"})
+
+@app.delete("/tasks/{id}", status_code=204)
+async def delete_task_by_id(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            tasks.remove(task)
+            return JSONResponse(status_code=204, content={"message": f"Task {id} deleted successfully."})
+    
+    return JSONResponse(status_code=404, content={"error": f"Task {id} not found"})
